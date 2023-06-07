365a021fcc36fee0253af9de330549a7f07b9a42
Barbara <128275198+brabaramor@users.noreply.github.com>
2023-06-07 12:02:28 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -2,11 +2,15 @@
 public class Produto{
     private String nome;
     private double preco;
+// etapa 2
+    private Data dataValidade;
     
 // Método Construtor
     public Produto (String nome, double preco){
         this.nome = nome;
         this.preco = preco;
+    //etapa 2
+        dataValidade = new Data();
     }
 
 //Método de acesso
@@ -18,6 +22,10 @@ public class Produto{
     public double getPreco(){
         return preco;
     }
+    //etapa 2
+    public Data getDataValidade(){
+        return dataValidade;
+    }
 
 //Set
     public void setNome(String nome){
@@ -27,10 +35,14 @@ public class Produto{
     public void setPreco(double preco){
         this.preco = preco;
     }
+    //etapa 2
+    public void setDataValidade(){
+        this.dataValidade = dataValidade;
+    }
 
 //Método toString
     public String toString() {
-        return "Nome: " + nome + "/ Preço: " + preco;
+        return "Nome: " + nome + "/ Preço: " + preco + "/ Data de validade: " + dataValidade;
     }
 }
 /* Fim da Etapa 1 */
