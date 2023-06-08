3e544e761924f5a7c2d5c50ee6d9538ddd89fda1
Barbara <128275198+brabaramor@users.noreply.github.com>
2023-06-08 13:05:30 -0300

    atualização com os métodos get e set de todos os atributos (sub e super classe)

--- Cosmetico.java
+++ Cosmetico.java
@@ -1,3 +1,62 @@
-public class Cosmetico{
-   
+public class Cosmetico extends Loja{
+    double taxaComercializacao;
+
+    //Construtor
+    public Cosmetico(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao, double taxaComercializacao){
+        
+        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao);
+        this.taxaComercializacao = taxaComercializacao;
+
+    }
+
+    //métodos de acesso
+    //get
+    public String getNome(){
+        return nome;
+    }
+
+    public int getQuantidadeFuncionarios(){
+        return quantidadeFuncionarios;
+    }
+
+    public double getSalarioBaseFuncionario(){
+        return salarioBaseFuncionario;
+    }
+
+    public Endereco getEndereco(){
+        return endereco;
+    }
+
+    public Data getDataFundacao(){
+        return dataFundacao;
+    }
+
+    public double getTaxaComercializacao(){
+        return taxaComercializacao;
+    }
+
+    //set
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setQuantidadeFuncionarios(int quantidadeFuncionarios){
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+    }
+
+    public void setSalarioBaseFuncionario(double salarioBaseFuncionario){
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+    public void setEndereco(Endereco endereco){
+        this.endereco = endereco;
+    }
+
+    public void setDataFundacao(Data dataFundacao){
+        this.dataFundacao = dataFundacao;
+    }
+
+    public void setTaxaComercializacao(double taxaComercializacao){
+        this.taxaComercializacao = taxaComercializacao;
+    }
 }
