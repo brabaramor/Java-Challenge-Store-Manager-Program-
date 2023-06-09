3220dee8bd759b7bc427fbe9c33815a6bab0096c
Mormata <barbara.mor@outlook.com>
2023-06-09 00:59:15 -0300

    etapa4 incompleta

--- Loja.java
+++ Loja.java
@@ -6,21 +6,27 @@ public class Loja{
 //etapa 2
     private Endereco endereco;
     private Data dataFundacao;
+//etapa 4
+    private Produto[] estoqueProdutos;
 
 //Metódos Construtores 
-    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao){
+    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao, Produto[] estoqueProdutos){
         this.nome = nome;
         this.quantidadeFuncionarios = quantidadeFuncionarios;
         this.salarioBaseFuncionario = salarioBaseFuncionario;
         //etapa 2
         this.endereco = endereco;
         this.dataFundacao = dataFundacao;
+        //etapa 4
+        this.estoqueProdutos = new Produto[10];
     }
 
-    public Loja(String nome, int quantidadeFuncionarios, Endereco endereco, Data dataFundacao){
+    public Loja(String nome, int quantidadeFuncionarios, Endereco endereco, Data dataFundacao, Produto[] estoqueProdutos){
         this.nome = nome;
         this.quantidadeFuncionarios = quantidadeFuncionarios;
-        this.salarioBaseFuncionario = -1;
+        salarioBaseFuncionario = -1.0;
+        this.estoqueProdutos = new Produto[10];
+
     }
 
 //Métodos de acesso
@@ -46,6 +52,11 @@ public class Loja{
         return endereco;
     }
 
+    //etapa4
+    public Produto[] getEstoqueProdutos(){
+        return estoqueProdutos;
+    }
+
 //Set
     public void setNome(String nome){
         this.nome = nome;
@@ -67,10 +78,14 @@ public class Loja{
     public void setEndereco(Endereco endereco){
         this.endereco = endereco;
     }
+    // etapa 4
+    public void setEstoqueProdutos(Produto[] estoqueProdutos){
+        this.estoqueProdutos = estoqueProdutos;
+    }
 
 //Método toString
     public String toString() {
-        return "Nome: " + nome + "/ Quantidade de Funcionários: " + quantidadeFuncionarios + "/ Salário Base dos Funcionários: " + salarioBaseFuncionario + "/ Data de Fundação: " + dataFundacao + "/ Endereço da Loja: " + endereco;
+        return "Nome: " + nome + "/ Quantidade de Funcionários: " + quantidadeFuncionarios + "/ Salário Base dos Funcionários: " + salarioBaseFuncionario + "/ Data de Fundação: " + dataFundacao + "/ Endereço da Loja: " + endereco + "/ Estoque de Produtos: " + estoqueProdutos;
     }
 
 //Método gastosComSalario
@@ -91,5 +106,16 @@ public class Loja{
             return 'G';
         }
     }
-}
+//etapa 4
+    public void imprimeProdutos(){
+        System.out.println("Estes são os produtos do estoque: ");
+        //criar novo Produto da classe produto para armazenar o nome que o usuario colocar
 
+        for (Produto produto : estoqueProdutos){
+            if (produto != null){
+            System.out.println(produto);
+            }
+        }
+
+    }
+}
